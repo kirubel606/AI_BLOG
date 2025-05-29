9685918bc4bae181b729fe0923882b5be59ace2d
KIRUBEL_TM <82014475+kirubel606@users.noreply.github.com>
2025-05-29 14:10:00 +0800

    ..

--- backend/quotes/urls.py
+++ backend/quotes/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import QuoteListCreateView, QuoteRetrieveUpdateDestroyView
+from .views import QuoteListCreateView, QuoteRetrieveUpdateDestroyView,TestimonialListView
 
 urlpatterns = [
+    path('testimonials/', TestimonialListView.as_view(), name='testimonials'),
     path('', QuoteListCreateView.as_view(), name='quote-list'),
     path('<int:pk>/', QuoteRetrieveUpdateDestroyView.as_view(), name='quote-detail'),
 ]
--- backend/quotes/views.py
+++ backend/quotes/views.py
@@ -5,7 +5,15 @@ from .serializers import QuoteSerializer
 class QuoteListCreateView(generics.ListCreateAPIView):
     queryset = Quote.objects.all()
     serializer_class = QuoteSerializer
+    def get_queryset(self):
+        return Quote.objects.filter(is_testimony=False)
 
 class QuoteRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Quote.objects.all()
     serializer_class = QuoteSerializer
+    
+class TestimonialListView(generics.ListAPIView):
+    serializer_class = QuoteSerializer
+
+    def get_queryset(self):
+        return Quote.objects.filter(is_testimony=True)
