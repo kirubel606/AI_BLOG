40b97c80f76b2fa6b4df5a5f6ca30ce07bed0081
KIRUBEL_TM <82014475+kirubel606@users.noreply.github.com>
2025-06-17 11:40:13 +0300

    rnd

--- backend/rnd/serializers.py
+++ backend/rnd/serializers.py
@@ -1,7 +1,16 @@
+# serializers.py
 from rest_framework import serializers
 from .models import RND
 
 class RndSerializer(serializers.ModelSerializer):
+    # Convert images comma string into list for output
+    images = serializers.SerializerMethodField()
+
     class Meta:
         model = RND
         fields = '__all__'
+
+    def get_images(self, obj):
+        if obj.images:
+            return obj.images.split(',')
+        return []
--- backend/rnd/views.py
+++ backend/rnd/views.py
@@ -1,11 +1,44 @@
+# views.py
+import os
+from django.core.files.storage import default_storage
+from rest_framework.parsers import MultiPartParser, FormParser
 from rest_framework import generics
-from .models import RND  # Make sure you have a model named Rnd
-from .serializers import RndSerializer  # Make sure you have a serializer for Rnd
+from .models import RND
+from .serializers import RndSerializer
 
 class RndListCreateView(generics.ListCreateAPIView):
     queryset = RND.objects.all()
     serializer_class = RndSerializer
+    parser_classes = [MultiPartParser, FormParser]
+
+    def perform_create(self, serializer):
+        images_files = self.request.FILES.getlist("images")
+        image_urls = []
+
+        for image_file in images_files:
+            path = default_storage.save(os.path.join("rnd", image_file.name), image_file)
+            url = default_storage.url(path)
+            image_urls.append(url)
+
+        # Save as comma separated string
+        images_str = ",".join(image_urls) if image_urls else ""
+
+        serializer.save(images=images_str)
 
 class RndRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     queryset = RND.objects.all()
     serializer_class = RndSerializer
+    parser_classes = [MultiPartParser, FormParser]
+
+    def perform_update(self, serializer):
+        instance = self.get_object()
+        images_files = self.request.FILES.getlist("images")
+        image_urls = instance.images.split(',') if instance.images else []
+
+        for image_file in images_files:
+            path = default_storage.save(os.path.join("rnd", image_file.name), image_file)
+            url = default_storage.url(path)
+            image_urls.append(url)
+
+        images_str = ",".join(image_urls) if image_urls else ""
+        serializer.save(images=images_str)
