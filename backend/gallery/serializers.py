cdb24f809157cd95e3f1db0ef3c27af79fd073e3
KIRUBEL_TM <82014475+kirubel606@users.noreply.github.com>
2025-06-20 15:33:48 +0300

    ..

--- backend/gallery/serializers.py
+++ backend/gallery/serializers.py
@@ -4,11 +4,45 @@ from .models import Gallery, GalleryImage
 class GalleryImageSerializer(serializers.ModelSerializer):
     class Meta:
         model = GalleryImage
-        fields = ['id', 'image', 'caption']
+        fields = ["id", "image"]
 
 class GallerySerializer(serializers.ModelSerializer):
-    images = GalleryImageSerializer(many=True, read_only=True)
+    images = GalleryImageSerializer(many=True, required=False)
 
     class Meta:
         model = Gallery
-        fields = ['id', 'title', 'created_at', 'images']
+        fields = ["id", "title", "caption", "images", "created_at"]
+
+    def create(self, validated_data):
+        images_data = self.context['request'].FILES.getlist('images')
+        gallery = Gallery.objects.create(**validated_data)
+        for image in images_data:
+            GalleryImage.objects.create(gallery=gallery, image=image)
+        return gallery
+
+    def update(self, instance, validated_data):
+        request = self.context['request']
+
+        removed_images = request.data.getlist("removed_images")
+        if removed_images:
+            from urllib.parse import urlparse
+
+            removed_paths = []
+            for url in removed_images:
+                parsed_url = urlparse(url)
+                path = parsed_url.path
+                if path.startswith("/media/"):
+                    path = path[len("/media/"):]
+                removed_paths.append(path)
+
+            instance.images.filter(image__in=removed_paths).delete()
+
+        images_data = request.FILES.getlist("images")
+        for image in images_data:
+            GalleryImage.objects.create(gallery=instance, image=image)
+
+        instance.title = validated_data.get("title", instance.title)
+        instance.caption = validated_data.get("caption", instance.caption)
+        instance.save()
+
+        return instance
