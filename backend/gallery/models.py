a179140cd311622c2d4f34cb2c899f394db09cb5
natkass <77274542+natkass@users.noreply.github.com>
2025-06-25 10:35:18 +0300

    gallery

--- backend/gallery/models.py
+++ backend/gallery/models.py
@@ -1,9 +1,17 @@
 from django.db import models
 
 class Gallery(models.Model):
+    CATEGORY_CHOICES = [
+        ('DATA_CENTER', 'Data Center'),
+        ('SHOWROOM', 'Showroom'),
+        ('SUMMER_CAMP', 'Summer Camp'),
+        ('MOU', 'MOU'),
+    ]
     title = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)  # ⏱️ Timestamp
     caption = models.CharField(max_length=400, blank=True)
+    category = models.CharField( max_length=100, choices=CATEGORY_CHOICES, null=True, blank=True    ) 
+    discription = models.TextField(null=True,blank=True)  # 📜 Description
 
 
     def __str__(self):
