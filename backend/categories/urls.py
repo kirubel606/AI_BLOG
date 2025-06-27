26a05a9e6eb769b635f1566d88ea5dc24bfe2979
KIRUBEL_TM <82014475+kirubel606@users.noreply.github.com>
2025-06-27 11:44:44 +0300

    Update urls.py

--- backend/categories/urls.py
+++ backend/categories/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import CategoryListCreateView, CategoryRetrieveUpdateDestroyView
 
 urlpatterns = [
-    path('all', CategoryListCreateView.as_view(), name='category-list-all'),
+    path('', CategoryListCreateView.as_view(), name='category-list-all'),
     path('core', CategoryListCreateView.as_view(), name='category-list-core'),
     path('non-core', CategoryListCreateView.as_view(), name='category-list-non-core'),
     path('<int:pk>/', CategoryRetrieveUpdateDestroyView.as_view(), name='category-detail'),
